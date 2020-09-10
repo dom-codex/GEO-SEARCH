15872a2ba4b432f3e67d4e531e143837a2a5c0b0
dom-codex <dominicwest7@gmail.com>
2020-09-10 18:19:51 +0100

    chore:add map static map view

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const openConverter = document.querySelector(".open-conv");
 const converter = document.querySelector(".calculator");
 const closeConverter = document.querySelector(".close");
+const reports = document.querySelector(".weather-reports");
+const map = document.querySelector(".mapview");
+const openMap = document.querySelector(".openMap");
+const closeMap = document.querySelector(".map-close");
+
 openConverter.addEventListener("click", () => {
   if (converter.classList.contains("openconverter")) {
     return converter.classList.remove("openconverter");
@@ -9,4 +14,14 @@ openConverter.addEventListener("click", () => {
 });
 closeConverter.addEventListener("click", () => {
   converter.classList.remove("openconverter");
+});
+openMap.addEventListener("click", () => {
+  reports.classList.add("fadeout");
+  map.classList.add("fadein");
+});
+closeMap.addEventListener("click", () => {
+  reports.classList.remove("fadeout");
+  map.classList.remove("fadein");
+  map.classList.add("fadeout");
+  reports.classList.add("fadein");
 });
